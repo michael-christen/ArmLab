4795d7c05722b82295b61e75e45a4ddc11732fc6
Edwin Olson <eblson@umich.edu>
2014-01-11 17:03:58 -0500

    audit and re-enable YUYV conversion

--- src/imagesource/image_convert.c
+++ src/imagesource/image_convert.c
@@ -151,26 +151,24 @@ static image_u32_t *convert_yu12_to_u32(image_source_data_t *frmd)
 
 static image_u32_t *convert_yuyv_to_u32(image_source_data_t *frmd)
 {
-    assert(0);
-
     image_u32_t *im = image_u32_create(frmd->ifmt.width,
                                        frmd->ifmt.height);
 
     int width = im->width;
     int height = im->height;
     int stride = im->stride;
-    uint32_t a = 0xff << 24;
 
     int sstride = width*2;
     uint8_t *yuyv = (uint8_t*)(frmd->data);
+    uint8_t *out = (uint8_t*) im->buf;
 
-    for (int i = 0; i < height; i++) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width / 2; x++) {
 
-        for (int j = 0; j < width / 2; j++) {
-            int y1 = yuyv[i*sstride + 4*j+0]&0xff;
-            int u  = yuyv[i*sstride + 4*j+1]&0xff;
-            int y2 = yuyv[i*sstride + 4*j+2]&0xff;
-            int v  = yuyv[i*sstride + 4*j+3]&0xff;
+            int y1 = yuyv[y*sstride + 4*x+0]&0xff;
+            int u  = yuyv[y*sstride + 4*x+1]&0xff;
+            int y2 = yuyv[y*sstride + 4*x+2]&0xff;
+            int v  = yuyv[y*sstride + 4*x+3]&0xff;
 
             int cb = ((u-128) * 454)>>8;
             int cr = ((v-128) * 359)>>8;
@@ -180,12 +178,20 @@ static image_u32_t *convert_yuyv_to_u32(image_source_data_t *frmd)
             r = clamp(y1 + cr);
             b = clamp(y1 + cb);
             g = clamp(y1 - cg);
-            im->buf[i*stride + 2*j+0] = (r) | (g<<8) | (b<<16) | a;
+
+            out[4*(y*stride+2*x) + 0] = r;
+            out[4*(y*stride+2*x) + 1] = g;
+            out[4*(y*stride+2*x) + 2] = b;
+            out[4*(y*stride+2*x) + 3] = 0xff;
 
             r = clamp(y2 + cr);
             b = clamp(y2 + cb);
             g = clamp(y2 - cg);
-            im->buf[i*stride + 2*j+1] = (r) | (g<<8) | (b<<16) | a;
+
+            out[4*(y*stride+2*x+1) + 0] = r;
+            out[4*(y*stride+2*x+1) + 1] = g;
+            out[4*(y*stride+2*x+1) + 2] = b;
+            out[4*(y*stride+2*x+1) + 3] = 0xff;
         }
     }
     return im;
@@ -193,8 +199,6 @@ static image_u32_t *convert_yuyv_to_u32(image_source_data_t *frmd)
 
 static image_u8x3_t *convert_yuyv_to_u8x3(image_source_data_t *frmd)
 {
-    assert(0);
-
     image_u8x3_t *im = image_u8x3_create(frmd->ifmt.width,
                                          frmd->ifmt.height);
 
@@ -204,14 +208,15 @@ static image_u8x3_t *convert_yuyv_to_u8x3(image_source_data_t *frmd)
 
     int sstride = width*2;
     uint8_t *yuyv = (uint8_t*)(frmd->data);
+    uint8_t *out = (uint8_t*) im->buf;
 
-    for (int i = 0; i < height; i++) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width / 2; x++) {
 
-        for (int j = 0; j < width / 2; j++) {
-            int y1 = yuyv[i*sstride + 4*j+0]&0xff;
-            int u  = yuyv[i*sstride + 4*j+1]&0xff;
-            int y2 = yuyv[i*sstride + 4*j+2]&0xff;
-            int v  = yuyv[i*sstride + 4*j+3]&0xff;
+            int y1 = yuyv[y*sstride + 4*x+0]&0xff;
+            int u  = yuyv[y*sstride + 4*x+1]&0xff;
+            int y2 = yuyv[y*sstride + 4*x+2]&0xff;
+            int v  = yuyv[y*sstride + 4*x+3]&0xff;
 
             int cb = ((u-128) * 454)>>8;
             int cr = ((v-128) * 359)>>8;
@@ -221,16 +226,18 @@ static image_u8x3_t *convert_yuyv_to_u8x3(image_source_data_t *frmd)
             r = clamp(y1 + cr);
             b = clamp(y1 + cb);
             g = clamp(y1 - cg);
-            im->buf[i*stride + 6*j+0] = r;
-            im->buf[i*stride + 6*j+1] = g;
-            im->buf[i*stride + 6*j+2] = b;
+
+            out[y*stride+6*x + 0] = r;
+            out[y*stride+6*x + 1] = g;
+            out[y*stride+6*x + 2] = b;
 
             r = clamp(y2 + cr);
             b = clamp(y2 + cb);
             g = clamp(y2 - cg);
-            im->buf[i*stride + 6*j+3] = r;
-            im->buf[i*stride + 6*j+4] = g;
-            im->buf[i*stride + 6*j+5] = b;
+
+            out[y*stride+6*x + 3] = r;
+            out[y*stride+6*x + 4] = g;
+            out[y*stride+6*x + 5] = b;
         }
     }
     return im;
